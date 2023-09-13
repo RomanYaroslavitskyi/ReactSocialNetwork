6df28fd493cf4849cd791f7f8e89e4b83a50ddc1
Roman <Chugaisters@gmail.com>
2023-09-13 17:48:46 +0300

    onClick

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,12 +3,18 @@ import s from "./MyPosts.module.css";
 import Post from './Post/Post';
 const MyPosts = (props) => {
   let postsElements = props.posts.map(p => <Post message={p.message} likeCounts={p.likeCount} />);
-    return ( <div className={s.content}>
+   
+  let newPostElement = React.createRef();
+  let addPost =() => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
+  return (<div className={s.content}>
       <div>
         My posts
         <div>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-          <button>Add post</button>
+          <textarea name="" id="" cols="30" rows="10" ref={newPostElement} ></textarea>
+        <button onClick={addPost} >Add post</button>
           <button>Remove</button>
         </div>
         {postsElements}
